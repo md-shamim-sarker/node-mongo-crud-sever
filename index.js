bbff08dd09b842ccf34f9ac6dd3dc3248aff75e8
shamimspro <shamimspro@gmail.com>
2022-11-05 20:23:56 +0600

    starter ready

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
+require('colors');
 
 const app = express();
 const port = 5000;
 
+app.get('/', (req, res) => {
+    res.send("This server is running fine!!!");
+});
+
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`.black.bgCyan);
 });
